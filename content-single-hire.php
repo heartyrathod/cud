40471f336fc4d84958134ac61ef6b0adbc9df797
heartyrathod <114145054+heartyrathod@users.noreply.github.com>
2023-04-21 14:19:35 +0530

    Create content-single-hire.php

---
+++ content-single-hire.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<main class="cud-main">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<?php require get_stylesheet_directory() . '/banner.php'; ?>
+
+		<div class="entry-content">
+			<?php
+			the_content();
+
+			wp_link_pages(
+				array(
+					'before'   => '<nav class="page-links" aria-label="' . esc_attr__('Page', 'twentytwentyone') . '">',
+					'after'    => '</nav>',
+					/* translators: %: Page number. */
+					'pagelink' => esc_html__('Page %', 'twentytwentyone'),
+				)
+			);
+			?>
+		</div>
+
+	</article>
+</main>
